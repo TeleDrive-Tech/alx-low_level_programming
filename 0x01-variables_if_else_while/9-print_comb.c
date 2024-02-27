86618380203befb50348245a56babb2dc28405b3
TeleDrive-Tech <apraiseno1@gmail.com>
2024-02-27 14:34:41 +0100

    9printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Return: 0 (Successful)
- */
-
 int main(void)
 {
     int i;
-    for (i = 10; i < 100; i++)
-    {
-        putchar('0' + i);
-    }
-    if (i < 100)
-    {
-        putchar(',');
-        putchar(' ');
-    }
-    putchar (10);
 
-    return (0);
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0');  // Print the current digit
+
+        if (i < 9)
+        {
+            putchar(',');   // Print the comma
+            putchar(' ');   // Print the space
+        }
+    }
+
+    putchar('\n');  // Print a newline character
+
+    return 0;
 }
