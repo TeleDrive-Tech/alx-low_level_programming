22d77a379afe48f65085d361ffd6835db58eb569
TeleDrive-Tech <apraiseno1@gmail.com>
2024-02-27 18:58:56 +0100

    Im_tired!

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,73 @@
 #include <stdio.h>
+
+void format(int c);
+void putspace(void);
+void space_comma(void);
+
+
 /**
- * main - Entry point
- * Return: 0 (successful)
-*/
+ *main - Entry Point
+ *Description: Write a program that prints all possible
+ *combinations of two two-digit numbers.
+ *Return: 0 (sucessful)
+ */
 int main(void)
 {
 	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = (i + 1); j < 100; j++)
 		{
-			if (i < 99 && j < 100)
-			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
-			}
-
-			if (i < 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			format(i);
+			putspace();
+			format(j);
+			if (i != 98 || j != 99)
+				space_comma();
 		}
 	}
-	putchar(10);
-
+	putchar('\n');
 	return (0);
+}
+
+
+
+/**
+  *putspace - helper frunction
+  *Return: no return value
+  */
+void putspace(void)
+{
+	putchar(' ');
+}
+
+
+
+/**
+  *format - helper function
+  *Return: no return value
+  *@c: integer parameter
+  */
+void format(int c)
+{
+	if (c > 9)
+	{
+		putchar('0' + (c / 10));
+		putchar('0' + (c % 10));
+	}
+	else
+	{
+		putchar('0');
+		putchar('0' + c);
+	}
+}
+
+/**
+  *space_comma - helper function
+  *Return: no return value
+  */
+void space_comma(void)
+{
+	putchar(',');
+	putchar(' ');
 }
