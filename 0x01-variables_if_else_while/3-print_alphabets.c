ffa0df1fe897ffccd1102696e32be615cc73b77c
TeleDrive-Tech <apraiseno1@gmail.com>
2024-02-27 10:56:49 +0100

    print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Successful)
+*/
+
+int main()
+{
+	char alphabet;
+
+	{
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		 putchar(alphabet);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
