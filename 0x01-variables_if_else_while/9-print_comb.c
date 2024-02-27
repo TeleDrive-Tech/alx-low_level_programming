14fe15f50f0fec026625210b22885197cc21911c
TeleDrive-Tech <apraiseno1@gmail.com>
2024-02-27 14:53:01 +0100

    9printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,11 @@ int main(void)
 	for (int i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar (10);
 	return (0);
